95203b4febb6b297f7a7feed9db9bf0d0c3ceee0
TheBrunster <70962135+TheBrunster@users.noreply.github.com>
2025-03-31 17:40:02 -0400

    Update ml_algorithms.py

--- src/ml_algorithms.py
+++ src/ml_algorithms.py
@@ -1,7 +1,8 @@
+import config
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 
-def predict_temperature(df):
+def predict_temperature(df=config.data):
     X = df.drop('Temperature', axis=1)  # Features
     y = df['Temperature']  # Target variable
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
